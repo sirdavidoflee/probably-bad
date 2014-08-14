082c13b008fa26dde5de25f668522fd2e0d6de88
Dave Lee <dlee@mullen.com>
2014-08-14 12:21:44 -0400

    updated home

--- themes/bad-theme/search.php
+++ themes/bad-theme/search.php
@@ -13,34 +13,48 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			 
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'bad-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			<h1 class="search-title"><?php printf( __( 'Search Results for: %s', 'bad-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			
-
-			<?php /* Start the Loop */ ?>
-<?php 
-				while ( have_posts() ) : the_post(); 
-				$title = $post->post_title;
-				$excerpt = wp_trim_words(get_the_excerpt(), '10');
-				$category = get_the_category($post->ID)[0];
-?>
-
-				<li>
+			<ul class="article-list">
 <?php
-				if(has_post_thumbnail()):
-?>
-					<a href="<? echo get_permalink() ?>">
-						<?php the_post_thumbnail('large'); ?>
-					</a>
-<?php
-				endif;
-?>
-					<h4><a href="<? echo get_permalink() ?>"><? echo $title ?></a></h4>
-					<p><? echo $excerpt ?></p>
-				</li>
+						while ( have_posts() ) : the_post();
+							$title = $post->post_title;
+							$excerpt = wp_trim_words(get_the_excerpt(), '30');
+				
+							$start = ($wp_query->query_vars['paged'] == 0) ? 1 : (($wp_query->query_vars['paged'] - 1) * $wp_query->query_vars['posts_per_page']) + 1;
+							$end = $start + ($wp_query->query_vars['posts_per_page'] - 1);
+							$end = ($end > $wp_query->found_posts)? $wp_query->found_posts : $end;
+							$total = $wp_query->found_posts;
+							//$category = get_the_category($post->ID)[0];
+			?>
 
-			<?php endwhile; ?>
+							<li>
+			<?php
+							if(has_post_thumbnail()):
+			?>
+								<a href="<? echo get_permalink() ?>">
+									<?php the_post_thumbnail('thumbnail'); ?>
+								</a>
+			<?php
+							endif;
+			?>
+								<h4><a href="<? echo get_permalink() ?>"><? echo $title ?></a></h4>
+								<p><? echo $excerpt ?></p>
+								<div class="meta">
+									<?php bad_theme_posted_on(); ?>
+								</div><!-- .entry-meta -->
+							</li>
 
-			<?php bad_theme_paging_nav(); ?>
+						<?php endwhile; ?>
+					</ul>
+
+						<div class="pagination">
+							<ul class="prev-next">
+								<li class="prev"><?php previous_posts_link('&laquo; Prev', $wp_query->max_num_pages) ?></li>
+								<li class="next"><?php next_posts_link('Next &raquo;', $wp_query->max_num_pages) ?></li>
+							</ul>
+							<h4><?php echo $start . '-' . $end . ' of ' . $total ?></h4>
+						</div>
 
 		<?php else : ?>
 
