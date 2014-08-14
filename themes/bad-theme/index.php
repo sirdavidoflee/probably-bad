5ad8338fbd938a9caa2c982f9d3ff29116640e2e
Dave Lee <dlee@mullen.com>
2014-08-14 12:14:15 -0400

    updated home

--- themes/bad-theme/index.php
+++ themes/bad-theme/index.php
@@ -23,6 +23,11 @@ get_header(); ?>
 			while ( have_posts() ) : the_post();
 				$title = $post->post_title;
 				$excerpt = wp_trim_words(get_the_excerpt(), '30');
+				
+				$start = ($wp_query->query_vars['paged'] == 0) ? 1 : (($wp_query->query_vars['paged'] - 1) * $wp_query->query_vars['posts_per_page']) + 1;
+				$end = $start + ($wp_query->query_vars['posts_per_page'] - 1);
+				$end = ($end > $wp_query->found_posts)? $wp_query->found_posts : $end;
+				$total = $wp_query->found_posts;
 				//$category = get_the_category($post->ID)[0];
 ?>
 
@@ -46,7 +51,13 @@ get_header(); ?>
 			<?php endwhile; ?>
 		</ul>
 
-			<?php bad_theme_paging_nav(); ?>
+			<div class="pagination">
+				<ul class="prev-next">
+					<li class="prev"><?php previous_posts_link('&laquo; Prev', $wp_query->max_num_pages) ?></li>
+					<li class="next"><?php next_posts_link('Next &raquo;', $wp_query->max_num_pages) ?></li>
+				</ul>
+				<h4><?php echo $start . '-' . $end . ' of ' . $total ?></h4>
+			</div>
 
 		<?php else : ?>
 
