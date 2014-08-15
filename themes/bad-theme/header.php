a31403799a11552628067fa492f36944d1c505d5
David Lee <sirdavidoflee@gmail.com>
2014-08-14 22:16:25 -0400

    updated meta

--- themes/bad-theme/header.php
+++ themes/bad-theme/header.php
@@ -18,7 +18,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, width=device-width" />
+
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
